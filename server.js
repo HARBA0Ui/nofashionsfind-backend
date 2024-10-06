825d21f1b69630f29e9b002922575dd8999ff03f
Harbaoui Ghaith <89579957+HARBA0Ui@users.noreply.github.com>
2024-10-06 14:41:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app= express()
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: "https://nofashionfinds-front.vercel.app", 
     credentials: true
 }))
 
